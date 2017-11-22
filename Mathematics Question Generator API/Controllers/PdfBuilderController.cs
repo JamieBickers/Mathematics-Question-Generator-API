9be5f53520a3e841869cd4f8ec27a16d4fd56921
JamieBickers <bickersjamie@googlemail.com>
2017-11-22 02:05:49 +0000

    API can now create extremely basic pdf.

---
+++ Mathematics Question Generator API/Controllers/PdfBuilderController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MathematicsQuestionGeneratorAPI.Models.PdfBuilders;
+
+namespace MathematicsQuestionGeneratorAPI.Controllers
+{
+    [Produces("application/json")] //TODO: What does this do?
+    [Route("api/PdfBuilder")]
+    public class PdfBuilderController : Controller
+    {     
+        // POST: api/PdfBuilder
+        [HttpGet]
+        public void Post()
+        {
+            var pdfBuilder = new BasicPdfBuilder(new List<string>());
+            pdfBuilder.BuildPdf(@"C:\Users\Jamie\Desktop\MathematicsQuestionGeneratorAPI\testFile.pdf", "testFile.txt");
+        }
+    }
+}
---
+++ Mathematics Question Generator API/Models/PdfBuilders/BasicPdfBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using System.IO;
+using iTextSharp.text.pdf;
+
+namespace MathematicsQuestionGeneratorAPI.Models.PdfBuilders
+{
+    public class BasicPdfBuilder
+    {
+        public List<string> Questions { get; set; }
+
+        public BasicPdfBuilder(List<string> questions)
+        {
+            Questions = questions;
+        }
+
+        public void BuildPdf(string saveLocation, string documentName)
+        {
+            using (var file = File.Create(saveLocation))
+            {
+                var document = new Document(PageSize.A4, 10, 10, 10, 10);
+
+                var writer = PdfWriter.GetInstance(document, file);
+                document.Open();
+
+                var chunck = new Chunk("This is a chunck.");
+                var phrase = new Phrase("This is a phrase.");
+                var para = new Paragraph("This is a paragraph.");
+
+                document.Add(chunck);
+                document.Add(phrase);
+                document.Add(para);
+
+                var paragraph = new Paragraph();
+                paragraph.SpacingBefore = 10;
+                paragraph.SpacingAfter = 10;
+                paragraph.Alignment = Element.ALIGN_LEFT;
+                paragraph.Font = FontFactory.GetFont(FontFactory.HELVETICA, 12f, BaseColor.Green);
+                paragraph.Add("This is a formatted paragraph.");
+                document.Add(paragraph);
+
+                document.Close();
+            }
+        }
+    }
+}
